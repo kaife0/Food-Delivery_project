60385e55f170b51c2f2d0070d60cf988b585c2b7
Md Kaif <163404336+kaife0@users.noreply.github.com>
2025-03-22 12:12:38 +0530

    Update ExploreMenu.jsx

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -3,7 +3,7 @@ import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = ({ category, setCategory }) => {
-    return (
+    return ( 
         <div className='explore-menu' id='explore-menu' >
             <h1>Explore our menu</h1>
             <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time or more. </p>
