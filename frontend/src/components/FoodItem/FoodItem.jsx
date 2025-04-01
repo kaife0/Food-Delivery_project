897d0d75de2e6dbe113333e723e60d63819714e0
Md Kaif <163404336+kaife0@users.noreply.github.com>
2025-04-01 18:15:02 +0530

    Update FoodItem.jsx

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,7 +5,7 @@ import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({ id, name, price, description, image }) => {
 
-    const {cartItems, addToCart,removeFromCart} = useContext(StoreContext);
+    const {cartItems, addToCart,removeFromCart} = useContext(StoreContext); 
 
     return (
         <div className='food-item' >
